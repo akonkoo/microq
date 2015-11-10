c1cd30aee6313f84ca4d9167bc4e1c1aa48c064a
akonkoo <akonkoo@hotmail.com>
2015-11-10 17:49:11 +0800

    add email

--- micq/forms.py
+++ micq/forms.py
@@ -3,6 +3,14 @@
 from django import forms
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
+from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMultiAlternatives
+from django.template import loader
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+
 from .models import *
 
 
@@ -54,7 +62,6 @@ class QuestionForm(forms.ModelForm):
 
 class AnswerForm(forms.ModelForm):
     body = forms.CharField(label='回答', widget=forms.Textarea)
-    #question = forms.CharField(widget=forms.HiddenInput())
 
     class Meta:
         model = Answer
@@ -65,6 +72,125 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment 
         fields = ['body']
+
+
+class UserForm(forms.ModelForm):
+    url = forms.URLField(label='微博')
+    desc = forms.CharField(label='个人简介', widget=forms.Textarea)
+
+    def clean_url(self):
+        url = self.cleaned_data.get('url')
+        url = 'http://' + url
+        return url
+
+    class Meta:
+        model = MyUser
+        fields = ['username', 'sex', 'url', 'desc']
+
+
+class SetPasswordForm(forms.Form):
+    error_messages = {
+    'password_mismatch': u"两次密码不一致"
+    }
+    new_password1 = forms.CharField(label="新密码",
+                                    widget=forms.PasswordInput)
+    new_password2 = forms.CharField(label="确认密码",
+                                    widget=forms.PasswordInput)
+
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
+        super(SetPasswordForm, self).__init__(*args, **kwargs)
+
+    def clean_new_password2(self):
+        password1 = self.cleaned_data.get('new_password1')
+        password2 = self.cleaned_data.get('new_password2')
+        if password1 and password2:
+            if password1 != password2:
+                raise forms.ValidationError(
+                            self.error_messages['password_mismatch'],
+                            code='password_mismatch')
+
+    def save(self, commit=True):
+        self.user.set_password(self.cleaned_data['new_password1'])
+        if commit:
+            self.user.save()
+        return self.user
+
+
+class PasswordChangeForm(SetPasswordForm):
+    error_messages = dict(SetPasswordForm.error_messages, **{
+                        'password_incorrect':u"旧密码不正确，请重新输入"
+                        })
+    old_password = forms.CharField(label="旧密码",
+                                    widget=forms.PasswordInput)
+    def clean_old_password(self):
+        #验证旧密码
+        old_password = self.cleaned_data['old_password']
+        if not self.user.check_password(old_password):
+            raise forms.ValidationError(
+                        self.error_messages['password_incorrect'],
+                        code='password_incorrect')
+        return old_password
+
+
+class PasswordResetForm(forms.Form):
+    email = forms.EmailField(label='Email', max_length=254)
+
+    def send_email(self, subject_template_name, email_template_name, 
+                    context, from_email, to_email, html_email_template_name=None):
+        subject = loader.render_to_string(subject_template_name,
+                                        context)
+        #Email Subject 不能有换行符
+        subject = ''.join(subject.splitlines())
+        body = loader.render_to_string(email_template_name, context)
+        email_message = EmailMultiAlternatives(subject, body,
+                                            from_email, [to_email])
+        if html_email_template_name is not None:
+            html_email = loader.render_to_string(html_email_template_name, context)
+            email_message.attach_alternative(html_email, 'text/html')            
+        email_message.send()
+
+    def get_users(self, email):
+        #判断是否活跃用户
+        active_users = get_user_model()._default_manager.filter(
+                        email__iexact=email, is_active=True)
+        return (u for u in active_users if u.has_usable_password())
+
+    def save(self, domain_override=None,
+            subject_template_name='micq/email/password_reset_subject.txt',
+            email_template_name='micq/email/password_reset_email.html',
+            use_https=False,
+            token_generator=default_token_generator,
+            from_email=None, request=None,
+            html_email_template_name=None):
+        #生成一个一次性使用唯一的链接，重置密码，并发送至用户
+
+        email = self.cleaned_data['email']
+
+        for user in self.get_users(email):
+            if not domain_override:
+                current_site = get_current_site(request)
+                site_name = current_site.name
+                domain = current_site.domain
+            else:
+                site_name = domain = domain_override
+                
+            context = {
+                'email': user.email,
+                'domain': domain,
+                'site_name': site_name,
+                #将user.pk解析成字符串，再编码以便在URL中使用
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'user': user,
+                #生成加密验证数据
+                'token': token_generator.make_token(user),
+                'protocol': 'https' if use_https else 'http',
+            }
+
+            self.send_email(subject_template_name, 
+                            email_template_name, context, from_email, user.email,
+                            html_email_template_name=html_email_template_name)
+
 
 
 
--- micq/views.py
+++ micq/views.py
@@ -1,15 +1,22 @@
 #coding: utf-8
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, resolve_url
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
+
+
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.response import TemplateResponse
+from django.utils.encoding import force_text
+from django.utils.http import is_safe_url, urlsafe_base64_decode
 
 from .models import *
 from .forms import *
 
-from django.views.generic.edit import UpdateView
 
 
 def index(request):
@@ -59,16 +66,16 @@ def question_detail(request, pk):
     except:
         return HttpResponseRedirect('micq/fail.html')
 
-    answer_form = AnswerForm()
-
     answer_list = Answer.objects.filter(question=question)
-    #tag_list = Tag.objects.filter(question=question)
     question_comment_list = Comment.objects.filter(question__pk=pk)
     answer_comment_list = Comment.objects.filter(answer__question__pk=pk)
-    c_q = False
+
+    answer_form = AnswerForm()
+    comment_form = CommentForm()
+
     return render(request, 'micq/question_detail.html', locals())
 
-
+#question
 @login_required()
 def create_question(request):
     if request.method == 'POST':
@@ -132,6 +139,7 @@ def question_update(request, pk):
         return render(request, 'micq/create_question.html', {'form': form})
 
 
+#answer
 @login_required()
 def create_answer(request, pk):
     if request.method == 'POST':
@@ -166,9 +174,11 @@ def answer_update(request, pk, id):
             return HttpResponseRedirect(reverse('question_detail', args=[pk]))
         else:
             form = AnswerForm(instance=answer)
-        return render(request, 'micq/answer_detail.html', {'question':question,
-                                    'form': form,
-                                    'answer_update': True})
+        return render(request, 'micq/answer_detail.html', {
+                                'question':question,
+                                'form': form,
+                                'answer_update': True        #修改答案框开关
+                                })
 
 
 @login_required
@@ -180,22 +190,194 @@ def answer_delete(request, pk, id):
         return HttpResponseRedirect(reverse('question_detail', args=[pk])) 
 
 
-def create_comment(request, pk):
+@login_required
+def question_comment_create(request, pk):
     if request.method == 'POST':
         comment_form = CommentForm(request.POST)
         if comment_form is not None and comment_form.is_valid():
-            comment = Comment.objects.create(body=comment_form.cleaned_data['bdoy'],
+            comment = Comment.objects.create(body=comment_form.cleaned_data['body'],
                                             user=request.user)
             comment.save()
-            comment.question = Question.objects.get(pk=pk)
+            if comment_q:
+                comment.question = Question.objects.get(pk=pk)
+            elif comment_a:
+                comment.answer = Answer.objects.get(question__pk=pk)
             comment.save()
         return HttpResponseRedirect(reverse('question_detail', args=[pk]))
     else:
         comment_form = CommentForm()
-        c_q = True
-        return HttpResponseRedirect(reverse('question_detail', args=[pk])) 
+        return render(request, 'micq/question_detail.html', locals())
 
 
+@login_required
+def question_comment_delete(request, pk, id):
+    question = Question.objects.get(pk=pk)
+    comment = Comment.objects.get(id=id)
+    comment.delete()
+    return HttpResponseRedirect(reverse('question_detail', args=[pk]))
+
+@login_required
+def users(request):
+    user = request.user
+    question_list = Question.objects.filter(user=user)
+    answer_list = Answer.objects.filter(user=user)
+    return render(request, 'micq/users.html', locals())
+
+
+@login_required
+def userchange(request):
+    user = request.user
+    if request.method == "POST":
+        form = UserForm(request.method or None, instance=user)
+        if form.is_valid():
+            form.save()
+        return render(request, 'micq/users.html', locals())
+    else:
+        form = UserForm(instance=user)
+    return render(request, 'micq/userchange.html', {'form':form})
+
+
+@login_required
+def password_change(request):
+    
+    if request.method == "POST":
+        form = PasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            
+            #防止更新密码之后登出会话
+            update_session_auth_hash(request, form.user)
+            return HttpResponseRedirect(reverse('users', args=[]))
+    else:
+        form = PasswordChangeForm(user=request.user)
+    return render(request, 'micq/password_change.html', {'form': form})
+
+
+def password_reset(request, is_admin_site=False,
+                    template_name='micq/password_reset_form.html',
+                    email_template_name='micq/email/password_reset_email.html',
+                    subject_template_name='micq/email/password_reset_subject.txt',
+                    password_reset_form=PasswordResetForm,
+                    token_generator=default_token_generator,
+                    post_reset_redirect=None,
+                    from_email=None,
+                    current_app=None,
+                    extra_context=None,
+                    html_email_template_name=None):
+    # if post_reset_redirect is None:
+    #     post_reset_redirect = reverse('password_reset_done')
+    # else:
+    #     post_reset_redirect = resolve_url(post_reset_redirect)
+    if request.method == 'POST':
+        form = password_reset_form(request.POST)
+        if form.is_valid():
+            opts = {
+                'use_https': request.is_secure(),
+                'token_generator': token_generator,
+                'from_email': from_email,
+                'email_template_name': email_template_name,
+                'request': request,
+                'html_email_template_name': html_email_template_name,
+            }
+            if is_admin_site:
+                warnings.warn(
+                    'django.contrib.auth.views.password_reset()'
+                    '的参数is_admin_site'
+                    '已取消，将在Django 2.0中删除。',
+                    RemovedInDjango20Warning, 3)
+                opts = dict(opts,
+                domain_override=request.get_host())
+            form.save(**opts)
+            return HttpResponseRedirect('/password_reset_done/')
+    else:
+        form = password_reset_form()
+        context = {
+            'form': form,
+            'title': 'Password reset',
+        }
+
+    if extra_context is not None:
+        context.update(extra_context)
+        
+    if current_app is not None:
+        request.current_app = current_app
+
+    return TemplateResponse(request, template_name, context)
+
+
+def password_reset_done(request):
+    return render(request, 'micq/password_reset_done.html', {
+                    'title': 'Password reset sent'})
+
+
+def password_reset_confirm(request, uidb64=None, token=None,
+                            template_name="micq/password_reset_confirm.html",
+                            token_generator=default_token_generator,
+                            post_reset_redirect=None,
+                            current_app=None, extra_context=None):
+
+    UserModel = get_user_model()
+    assert uidb64 is not None and token is not None  #check by URLconf
+
+    if post_reset_redirect is None:
+        post_reset_redirect = reverse('password_reset_complete')
+    else:
+        post_reset_redirect = resolve_url(post_reset_redirect)
+
+    try:
+        # urlsafe_base64_decode() decodes to bytestring on Python 3
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = UserModel._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, UserModel.DoesNotExist):
+        user = None
+
+    if user is not None and token_generator.check_token(user, token):
+        validlink = True
+        title = 'Enter new password'
+        if request.method == 'POST':
+            form = set_password_form(user, request.POST)
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect(post_reset_redirect)
+        else:
+            form = set_password_form(user)
+    else:
+        validlink = False
+        form = None
+        title = 'Password reset Unsuccessful'
+
+    context = {
+        'form': form,
+        'title': title,
+        'validlink' : validlink,
+    }
+
+    if extra_context is not None:
+        context.update(extra_context)
+
+    if current_app is not None:
+        request.current_app = current_app
+        
+    return TempalteResponse(request, template_name, context)
+
+
+def password_reset_complete(request, 
+                            template_name="mciq/password_reset_complete.html",
+                            current_app=None, extra_context=None):
+    context = {
+        'login_url' : resolve_url(settings.LOGIN_URL),
+        'title': 'Password reset complete',
+    }
+
+    if extra_context is not None:
+        context.update(extra_context)
+
+    if current_app is not None:
+        request.current_app = current_app
+
+    return TempalteResponse(request, template_name, context)
+
+    
 
         
             
